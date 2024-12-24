a9414ac124fc0f2a5a836239af78025be0773027
BukanChoki <terrorblade12341@gmail.com>
2024-12-24 17:28:36 +0700

    Renaming the old files

---
+++ old_app.py
@@ -0,0 +1,42 @@
+import old_bot_logic as ai_choice
+
+hand_gesture = ["Rock", "Paper", "Scissors"]
+user = False
+
+while user == False:
+    user = int(input("1.Rock  2.Paper  3.Scissors \nChoose your hand(1, 2, or 3)\n"))
+
+    computer = ai_choice.generate_hand()
+    # computer[0] is the number
+    # computer[1] is the hand_gesture
+
+    if user < 1 or user > 3:
+        print("Input choice exceed the desire limit (1 - 3), please try again\n")
+        user = False
+    else:
+        print("\n" + hand_gesture[user - 1] + " against " + computer[1])
+
+        # Player hands : Rock
+        if user - 1 == 0:
+            if computer[0] == 0:
+                print("It's a DRAW\n")
+            elif computer[0] == 1:
+                print("You WIN\n")
+            elif computer[0] == 2:
+                print("You LOSE\n")
+        # Player hands : Paper
+        elif user - 1 == 1:
+            if computer[0] == 0:
+                print("You WIN\n")
+            elif computer[0] == 1:
+                print("It's a DRAW\n")
+            elif computer[0] == 2:
+                print("You LOSE\n")
+        # Player hands : Scissors
+        elif user - 1 == 2:
+            if computer[0] == 0:
+                print("You LOSE\n")
+            elif computer[0] == 1:
+                print("You WIN\n")
+            elif computer[0] == 2:
+                print("It's a DRAW\n")
---
+++ old_bot_logic.py
@@ -0,0 +1,8 @@
+import random
+
+def generate_hand():
+    hand_gesture = ["Rock", "Paper", "Scissors"]
+    number_generated = random.randint(0, 2)
+    result = hand_gesture[number_generated]
+
+    return [number_generated, result]
