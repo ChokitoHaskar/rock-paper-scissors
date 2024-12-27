efe997b90f69492e39719469145c7f35b15cade2
BukanChoki <terrorblade12341@gmail.com>
2024-12-27 08:40:04 +0700

    delete bot_logic

--- bot_logic.py
+++
@@ -1,9 +0,0 @@
-import random
-
-
-def generate_hand():
-    hand_gesture = ["Rock", "Paper", "Scissors"]
-    number_generated = random.randint(0, 2)
-    result = hand_gesture[number_generated]
-
-    return [number_generated, result]
